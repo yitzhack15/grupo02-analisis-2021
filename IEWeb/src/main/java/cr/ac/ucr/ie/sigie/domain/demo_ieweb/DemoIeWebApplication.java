544c8bac0497ce95717d641dfbf8c7c30148218e
Ian <ian.ondoy@ucr.ac.cr>
2021-10-21 12:26:26 -0600

    Backend, Carrusel y Footer.

---
+++ IEWeb/src/main/java/cr/ac/ucr/ie/sigie/domain/demo_ieweb/DemoIeWebApplication.java
@@ -0,0 +1,13 @@
+package cr.ac.ucr.ie.sigie.domain.demo_ieweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoIeWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoIeWebApplication.class, args);
+    }
+
+}
---
+++ IEWeb/src/test/java/cr/ac/ucr/ie/sigie/domain/demo_ieweb/DemoIeWebApplicationTests.java
@@ -0,0 +1,13 @@
+package cr.ac.ucr.ie.sigie.domain.demo_ieweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoIeWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
