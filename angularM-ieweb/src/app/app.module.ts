a6c3ee792471eaa3475a97a3140a27e7290a96f3
Ian <ian.ondoy@ucr.ac.cr>
2021-10-21 02:14:31 -0600

    Encabezado y menú

--- angularM-ieweb/src/app/app.module.ts
+++ angularM-ieweb/src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [
+     AppRoutingModule,BrowserModule, FormsModule, FlexLayoutModule, BrowserAnimationsModule,
+     MatIconModule, MatButtonModule, MatSidenavModule, MatToolbarModule
+   ],
+  declarations: [ AppComponent,HomeComponent],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
